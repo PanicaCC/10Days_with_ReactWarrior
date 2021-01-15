b2f641878f8e3b1d851a48aa0a73c3a0c0975b09
PanicaCC <panicacc@gmail.com>
2021-01-16 01:39:32 +0200

    Updates

---
+++ precache-manifest.9c76236e6852764ee974333d43a4db65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b004312f67fc8a875ef",
+    "url": "/10Days_with_ReactWarrior/static/css/main.c883bd4b.chunk.css"
+  },
+  {
+    "revision": "1b004312f67fc8a875ef",
+    "url": "/10Days_with_ReactWarrior/static/js/main.f7735e4b.chunk.js"
+  },
+  {
+    "revision": "91246f86d02e37e8400c",
+    "url": "/10Days_with_ReactWarrior/static/js/runtime~main.04b2fbec.js"
+  },
+  {
+    "revision": "9feb315d2b006d4048ce",
+    "url": "/10Days_with_ReactWarrior/static/css/2.61bbec63.chunk.css"
+  },
+  {
+    "revision": "9feb315d2b006d4048ce",
+    "url": "/10Days_with_ReactWarrior/static/js/2.76229e01.chunk.js"
+  },
+  {
+    "revision": "56c4e17002eb45e239be23866fe50278",
+    "url": "/10Days_with_ReactWarrior/index.html"
+  }
+];
--- src/Utils/api.js
+++
@@ -1,7 +0,0 @@
-//https://www.themoviedb.org/
-//login: PanicaCC
-//Password: default
-
-export const API_KEY = '4173b7e14337bf6b8646c5299735bacf'
-export const API_URL = 'https://api.themoviedb.org/3/'
-//export const API_KEY_AUTH = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MTczYjdlMTQzMzdiZjZiODY0NmM1Mjk5NzM1YmFjZiIsInN1YiI6IjVmZmYwNGMzNTk0Yzk0MDAzZThmOTRmNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8Z8aUZZrONLDys4UoVXVZ62EJv7IUa4EgMVoALto398'
